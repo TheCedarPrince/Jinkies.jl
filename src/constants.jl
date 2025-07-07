f1097c2056605b42dcde150ab14f917560845c0f
TheCedarPrince <jacobszelko@gmail.com>
2025-07-06 22:54:45 -0400

    Added constants

---
+++ src/constants.jl
@@ -0,0 +1,10 @@
+const EPD_7_IN_5_WIDTH = 800
+const EPD_7_IN_5_HEIGHT = 480
+
+const EPD_RST_PIN = 17
+const EPD_DC_PIN = 25
+const EPD_CS_PIN = 8
+const EPD_PWR_PIN = 18
+const EPD_BUSY_PIN = 24
+const EPD_MOSI_PIN = 10
+const EPD_SCLK_PIN = 11
